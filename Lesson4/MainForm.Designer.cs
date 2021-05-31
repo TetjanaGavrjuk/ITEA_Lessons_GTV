4f9fc29f8b579f704f2be50489a301542079fc46
DESKTOP-4BHG8Q6\tanya <godsend2me77@gmail.com>
2021-05-31 16:26:59 +0300

    +Делагаты, лямбды, цепочки делегатов

--- Lesson4/MainForm.Designer.cs
+++ Lesson4/MainForm.Designer.cs
@@ -30,11 +30,22 @@
         {
             this.btnOk = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubt = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDevision = new System.Windows.Forms.Button();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblOperationName = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(287, 193);
+            this.btnOk.Location = new System.Drawing.Point(326, 318);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(151, 29);
             this.btnOk.TabIndex = 0;
@@ -46,22 +57,132 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(215, 58);
+            this.label1.Location = new System.Drawing.Point(45, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(432, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Lesson 4. Делегаты, события, лямбда-выражения";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Операнд1";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(42, 161);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(30, 27);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnMathOperation_Click);
+            // 
+            // btnSubt
+            // 
+            this.btnSubt.Location = new System.Drawing.Point(78, 161);
+            this.btnSubt.Name = "btnSubt";
+            this.btnSubt.Size = new System.Drawing.Size(30, 27);
+            this.btnSubt.TabIndex = 3;
+            this.btnSubt.Text = "-";
+            this.btnSubt.UseVisualStyleBackColor = true;
+            this.btnSubt.Click += new System.EventHandler(this.btnMathOperation_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(114, 161);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(30, 27);
+            this.btnMulti.TabIndex = 3;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMathOperation_Click);
+            // 
+            // btnDevision
+            // 
+            this.btnDevision.Location = new System.Drawing.Point(150, 161);
+            this.btnDevision.Name = "btnDevision";
+            this.btnDevision.Size = new System.Drawing.Size(30, 27);
+            this.btnDevision.TabIndex = 3;
+            this.btnDevision.Text = "/";
+            this.btnDevision.UseVisualStyleBackColor = true;
+            this.btnDevision.Click += new System.EventHandler(this.btnMathOperation_Click);
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(110, 99);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(81, 20);
+            this.txtOperand1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Операнд2";
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(110, 131);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(81, 20);
+            this.txtOperand2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Tag = "";
+            this.label4.Text = "Результат операции";
+            // 
+            // lblOperationName
+            // 
+            this.lblOperationName.AutoSize = true;
+            this.lblOperationName.Location = new System.Drawing.Point(162, 202);
+            this.lblOperationName.Name = "lblOperationName";
+            this.lblOperationName.Size = new System.Drawing.Size(0, 13);
+            this.lblOperationName.TabIndex = 6;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(110, 218);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(81, 20);
+            this.txtResult.TabIndex = 4;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 279);
+            this.ClientSize = new System.Drawing.Size(518, 390);
+            this.Controls.Add(this.lblOperationName);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.btnDevision);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnSubt);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnOk);
             this.Name = "MainForm";
             this.Text = "ITEA. Урок 4";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,6 +192,17 @@
 
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubt;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDevision;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblOperationName;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
 
--- Lesson4/MainForm.cs
+++ Lesson4/MainForm.cs
@@ -1,33 +1,147 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lesson4
 {
+
+    // объявление своего собственного делегата(аналог процедурного типа в  Delphi):
+    public delegate int MathOperation(int x, int y);
+
     public partial class MainForm : Form
     {
-        delegate void DlgNmProc(int id);
+        //Общение с пользователем
+        private Action<object> ByToUser;
+        private Action<object> HelloToUser;
 
+
+        #region Form
         public MainForm()
         {
             InitializeComponent();
         }
 
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Составим план приветствия
+            HelloToUser += (s) => MessageBox.Show("Привет!");  //лямбда-выражение
+            HelloToUser += (s) =>                              //лямбда-метод
+            {
+                MessageBox.Show("Представляем Вашему вниманию наш супер-калькулятор!");
+            };
+
+            // Составим план трогательной истории прощания
+            ByToUser += SayResult;
+            ByToUser += SayBestRegars;
+            ByToUser += SayBay;
+            ByToUser += (s) => {
+                MessageBox.Show("И это было последнее ...");
+            };
+
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            //Поприветствуем пользователя через 2 лямбды
+            HelloToUser(sender);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Расскажем пользователю трогательную историю прощания...
+            ByToUser(sender);
+        }
+        #endregion
+
         private void btnOk_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Пока!");
             Close();
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        #region MathOperation
+        private void btnMathOperation_Click(object sender, EventArgs e)
         {
-
+            //btnMathOperation_ClickCustomDlg(sender, e); //работаем через свой собственный делегат
+            btnMathOperation_ClickFunc(sender, e);        //работаем через стандартный делегат
         }
+
+
+        // применение стандартного делегата Func:
+        private void btnMathOperation_ClickFunc(object sender, EventArgs e)
+        {
+            Func<int, int, int> currMathOperation = null;
+            Button ClickedBtn = (sender as Button);
+
+            if (ClickedBtn.Text == "+")
+            {
+                currMathOperation = Add;
+            }
+            else if (ClickedBtn.Text == "-")
+            {
+                currMathOperation = Sub;
+            };
+
+            lblOperationName.Text = ClickedBtn.Text;
+            int Result;
+            Result = currMathOperation(Int32.Parse(txtOperand1.Text), Int32.Parse(txtOperand2.Text));
+            txtResult.Text = Result.ToString();
+        }
+
+
+        // применение своего собственного делегата:
+        private void btnMathOperation_ClickCustomDlg(object sender, EventArgs e)
+        {
+            MathOperation currMathOperation = null;
+            Button ClickedBtn = (sender as Button);
+
+            if (ClickedBtn.Text == "+")
+            {
+                currMathOperation = new MathOperation(Add);
+            }
+            else if (ClickedBtn.Text == "-")
+            {
+                currMathOperation = new MathOperation(Sub);
+            };
+
+            lblOperationName.Text = ClickedBtn.Text;
+            int Result;
+            Result = currMathOperation(Int32.Parse(txtOperand1.Text), Int32.Parse(txtOperand2.Text));
+            txtResult.Text = Result.ToString();
+        }
+
+        private int Add(int x, int y)
+        {
+            return (x + y);
+        }
+
+        private int Sub(int x, int y)
+        {
+            return (x - y);
+        }
+        #endregion
+
+        #region ByToUser
+        private void SayHello(object sender)
+        {
+            MessageBox.Show("И снова здаствуйте!");
+        }
+
+        private void SayResult(object sender)
+        {
+            MessageBox.Show("Результат последней операции был =" + txtResult.Text);
+        }
+
+        private void SayBestRegars(object sender)
+        {
+            MessageBox.Show("Ваше мнение очень важно для нас! Оставьте отзыв на нашем сайте!");
+        }
+
+        private void SayBay(object sender)
+        {
+            MessageBox.Show("До новых встреч!");
+        }
+        #endregion
+
     }
+
+
 }
